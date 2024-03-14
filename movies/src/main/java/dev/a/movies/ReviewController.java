60737125a0cca9a991e70811f464a95ca9b98329
avishakr kotkar <kotkaravi07@gmail.com>
2024-03-14 16:13:08 +0530

    review controller defined

---
+++ movies/src/main/java/dev/a/movies/ReviewController.java
@@ -0,0 +1,26 @@
+package dev.a.movies;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(
+                reviewService.ceateReview(payload.get("reviewBody"), payload.get("imdbId")),
+                HttpStatus.CREATED);
+    }
+
+
+}
